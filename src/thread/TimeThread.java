5b6b06324b53005fe4f7f4df65610e3dc7339c98
jszbm <jorgenszachowicz@gmail.com>
2025-06-03 19:19:30 +0200

    Change timer implementation

--- src/thread/TimeThread.java
+++ src/thread/TimeThread.java
@@ -4,13 +4,13 @@ import java.util.concurrent.TimeUnit;
 
 public class TimeThread extends Thread {
 
-    int time = 0;
+    int time;
 
     @Override
     public void run() {
         while (true) {
             try {
-                TimeUnit.MILLISECONDS.sleep(1);
+                TimeUnit.SECONDS.sleep(1);
                 time++;
             } catch (InterruptedException e) {
                 break;
@@ -18,11 +18,8 @@ public class TimeThread extends Thread {
         }
     }
 
-    public int getTime() {
-        return time;
-    }
-
     public String getStringTime() {
-        return String.format("%d:%02d.%02d", time / 60000, time % 60000 / 1000, time % 1000);
+
+        return String.format("%02d:%02d", time / 60, time);
     }
 }
