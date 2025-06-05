5801e0246062b612760f44443e5fe202a5197c34
jszbm <jorgenszachowicz@gmail.com>
2025-06-05 18:57:46 +0200

    Update score system

--- src/model/score/ScoreMap.java
+++ src/model/score/ScoreMap.java
@@ -2,14 +2,33 @@ package model.score;
 
 
 import java.io.Serializable;
-import java.util.HashMap;
+import java.util.*;
 
 public class ScoreMap implements Serializable {
 
-    HashMap<String, Integer> scoreMap = new HashMap<>();
+    HashMap<String, Integer> scoreMap = new LinkedHashMap<>();
 
     public HashMap<String, Integer> getMap() {
+        sort();
         return scoreMap;
     }
 
+    public void putScore(String name, Integer score) {
+        scoreMap.put(name, score);
+        sort();
+    }
+
+    private void sort() {
+        List<Map.Entry<String, Integer>> entries = new ArrayList<>(scoreMap.entrySet());
+
+        entries.sort(Map.Entry.comparingByValue());
+
+        HashMap<String, Integer> sortedMap = new LinkedHashMap<>();
+
+        for(Map.Entry<String, Integer> entry : entries){
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+
+        scoreMap = sortedMap;
+    }
 }
